3b2841055be365a159a0f0c054dd2d77fa373577
ninanxiaoguai <19433616+ninanxiaoguai@users.noreply.github.com>
2018-04-12 23:55:21 +0800

    Update exercise_1.py

--- exercise_1.py
+++ exercise_1.py
@@ -30,14 +30,14 @@ for fname in images:
         # 将角点在图像上显示
         cv2.drawChessboardCorners(img, (w,h), corners, ret)
         cv2.imshow('findCorners',img)
-        cv2.waitKey(10)
+        cv2.waitKey(100000)
 cv2.destroyAllWindows()
 #
 ## 标定
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
 ############################################
-## 去畸变
+## 方法 1：undistort
 img2 = cv2.imread('left02.jpg')
 h,  w = img2.shape[:2]
 newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),0,(w,h)) # 自由比例参数
@@ -49,7 +49,7 @@ cv2.imwrite('undistort.png',dst)
 ##############################################
 
 ##############################################
- ##undistort
+ ##方法 2：remap
 mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,None,newcameramtx,(w,h),5)
 dst = cv2.remap(img,mapx,mapy,cv2.INTER_LINEAR)
 
